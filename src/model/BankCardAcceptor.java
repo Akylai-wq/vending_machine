350b225eea6d395ec83dfca939fb6e27276c176b
Akylai-wq <akylaia870@gmail.com>
2024-12-15 19:17:27 +0600

    Added the card number, password and balance, created a constructor, and getters and setters to BankCardAcceptor.java

--- src/model/BankCardAcceptor.java
+++ src/model/BankCardAcceptor.java
@@ -1,3 +1,35 @@
-public class BankCardAcceptor {
+package model;
 
+public class BankCardAcceptor {
+    private int bankCardNumber;
+    private int passwordCard;
+    private int amount;
+
+    public BankCardAcceptor(int amount) {
+        this.amount = amount;
+    }
+
+    public int getBankCardNumber() {
+        return bankCardNumber;
+    }
+
+    public void setBankCardNumber(int bankCardNumber) {
+        this.bankCardNumber = bankCardNumber;
+    }
+
+    public int getPasswordCard() {
+        return passwordCard;
+    }
+
+    public void setPasswordCard(int passwordCard) {
+        this.passwordCard = passwordCard;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
