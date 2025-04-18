8f836d326796746b7523460df79805ca2861c4b7
Akylai-wq <akylaia870@gmail.com>
2025-04-18 16:13:16 +0600

    Add enum for payment method and class for banknote

---
+++ src/model/BanknoteAcceptor.java
@@ -0,0 +1,17 @@
+package model;
+
+public class BanknoteAcceptor {
+    private int amount;
+
+    public BanknoteAcceptor(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
